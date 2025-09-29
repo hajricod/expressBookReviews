63b3b41a1e7dd7ce685d219961d89f3508d2cf83
hajricod <48984962+hajricod@users.noreply.github.com>
2025-09-29 22:59:49 +0400

    Refactor login route for improved readability and consistency

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -35,25 +35,25 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 regd_users.post("/login", (req,res) => {
   //Write your code here
   const username = req.body.username;
-    const password = req.body.password;
+  const password = req.body.password;
     // Check if username or password is missing
     if (!username || !password) {
       return res.status(404).json({ message: "Error logging in" });
     }
     // Authenticate user
-    if (authenticatedUser(username, password)) {
-        // Generate JWT access token
-        let accessToken = jwt.sign({
-          data: password
-        }, 'access', { expiresIn: 60 * 60 });
-        // Store access token and username in session
-        req.session.authorization = {
-          accessToken, username
-        }
-        return res.status(200).send("User successfully logged in");
-    } else {
-      return res.status(208).json({ message: "Invalid Login. Check username and password" });
+  if (authenticatedUser(username, password)) {
+      // Generate JWT access token
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
+    // Store access token and username in session
+    req.session.authorization = {
+      accessToken, username
     }
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({ message: "Invalid Login. Check username and password" });
+  }
 });
 
 // Add a book review
