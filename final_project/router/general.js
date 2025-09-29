e926c5fed1d1cd8b171b69f02fe5ecce124b9138
hajricod <48984962+hajricod@users.noreply.github.com>
2025-09-29 22:21:45 +0400

    modified get books and added get book by isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,13 +12,20 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.send(JSON.stringify(books,null,4));
+  const titles = Object.values(books).map(book => book.title);
+  res.json(titles);
+  // res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  if (book) {
+    res.json(book);
+  } else {
+    res.status(404).json({message: "Book not found"});
+  }
 });
   
 // Get book details based on author
